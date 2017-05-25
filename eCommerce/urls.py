af251cf7c8b822b7c54147cb2160ff335052a59d
Andriy-sth <sht.andriy@gmail.com>
2017-05-25 19:50:23 +0300

    allauth login

--- eCommerce/urls.py
+++ eCommerce/urls.py
@@ -13,21 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf import  settings
-from django.conf.urls.static import  static
-from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url, include
 from django.contrib import admin
 from profiles import views as profiles_views
 from contact import views as contact_views
-
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', profiles_views.home, name='home'),
     url(r'^about/$', profiles_views.About.as_view(), name='about'),
-    url(r'^contacts/$', contact_views.contact, name='contact')
+    url(r'^contacts/$', contact_views.contact, name='contact'),
+    url(r'^accounts/', include('allauth.urls')),
+
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
